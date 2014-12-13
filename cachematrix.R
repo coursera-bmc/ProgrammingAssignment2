9297fce97b545b7d37ccd11784ead066ee395ac8
coursera-bmc <coursera-bmc@users.noreply.github.com>
2014-12-13 15:07:33 -0500

    Update of cacheMatrix.R
    Final version of cacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Author: Brian McNally
+## Version: 1.0
+## Date: 2014.12.13
+## This R Module is for creating a matrix "super object" which contains both the matrix and 
+## its inverse.
+## Function makeCacheMatrix() is the constructor and contains access API
+## Function cacheSolve() MUST be called to populate inverse matrix on makeCacheMatrix object
 
-## Write a short comment describing this function
+## Creates a matrix "super object" which contains both the matrix and its inverse
+## Input: Matrix()
+## Access functions:
+##  get(): Return the input matrix
+##  setinverse(): <PRIVATE> Set the inverse of the matrix
+##  getinverse(): Return the inverse of the matrix or NULL if not set
+##  set(): Sets a new matrix to the object and sets inverse to NULL
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cachedInverse <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    cachedInverse <<- NULL
+  }
+  
+  get <- function()
+  {
+    x
+  }
+  
+  setinverse <- function(invertedMatrix)
+  {
+    cachedInverse <<- invertedMatrix
+  }
+  
+  getinverse <- function()
+  {
+    cachedInverse
+  }
+  
+  list(get = get, 
+       set = set, 
+       setinverse = setinverse, 
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## CacheSolve is called to set the inverse of the makeCacheMatrix object
+## Input: Object result of function makeCacheMatrix
+## If the inverse of the matrix has already been stored, cachesolve will 
+## print a message and return the stored result
+## Otherwise, it will calculate, store, and return the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  
+  storedInverse <- x$getinverse()
+  
+  if(!is.null(storedInverse))
+  {
+    message("getting cached data")
+    return(storedInverse)
+  }
+  
+  # The next three lines: Get the matrix data, Set the cacheInverse to the Inverse, Return
+  matrixData <- x$get()
+  x$setinverse(solve(matrixData))
+  return(x$getinverse())
 }
